bce0a1ea0e69ade4bf6a00c9e12223fece33b6d6
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2022-08-22 21:53:13 -0300

    finalizado

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import { defaultTheme, defaultTheme } from '../styles/themes/default'
+import 'styled-components'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
